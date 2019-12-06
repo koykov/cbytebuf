a6c96ec9acb5adce5dde66bc2151e104cff21dd5
Konstantin Voykov <koykov0@gmail.com>
2019-12-07 00:11:59 +0200

    Code formatting issue.

--- cbytebuf.go
+++ cbytebuf.go
@@ -21,10 +21,10 @@ const (
 
 // Variable-size alloc-free buffer.
 type CByteBuf struct {
-	sh reflect.SliceHeader // header to fast slice construct
-	b  []byte              // buffer slice
-	l  int                 // temporary int var
-	e  uint                // error code
+	h reflect.SliceHeader // header to fast slice construct
+	b []byte              // buffer slice
+	l int                 // temporary int var
+	e uint                // error code
 }
 
 var errs = []error{
@@ -41,32 +41,32 @@ func NewCByteBuf() (*CByteBuf, error) {
 
 // Get length of the buffer.
 func (b *CByteBuf) Len() int {
-	return b.sh.Len
+	return b.h.Len
 }
 
 // Get capacity of the buffer.
 func (b *CByteBuf) Cap() int {
-	return b.sh.Cap
+	return b.h.Cap
 }
 
 // Implement io.ReaderFrom.
 func (b *CByteBuf) ReadFrom(r io.Reader) (n int64, err error) {
-	if b.sh.Cap == 0 {
+	if b.h.Cap == 0 {
 		if err = b.Grow(64); err != nil {
 			return 0, err
 		}
 	}
 	for {
-		if b.sh.Len == b.sh.Cap {
-			if err = b.Grow(b.sh.Cap * 2); err != nil {
+		if b.h.Len == b.h.Cap {
+			if err = b.Grow(b.h.Cap * 2); err != nil {
 				return 0, err
 			}
 		}
-		b.l, err = r.Read(b.b[b.sh.Len:])
+		b.l, err = r.Read(b.b[b.h.Len:])
 		if b.l < 0 {
 			return n, errs[errNegativeRead]
 		}
-		b.sh.Len += b.l
+		b.h.Len += b.l
 		n += int64(b.l)
 		if err == io.EOF {
 			return n, nil
@@ -88,22 +88,22 @@ func (b *CByteBuf) Write(data []byte) (int, error) {
 	b.l = len(data)
 	if b.b == nil {
 		// First write, need to create internal byte slice.
-		b.sh.Cap = b.l * 2
+		b.h.Cap = b.l * 2
 		ptrErr := (*C.uint)(unsafe.Pointer(&b.e))
-		ptrAddr := (*C.uintptr)(unsafe.Pointer(&b.sh.Data))
-		ptrCap := (*C.int)(unsafe.Pointer(&b.sh.Cap))
+		ptrAddr := (*C.uintptr)(unsafe.Pointer(&b.h.Data))
+		ptrCap := (*C.int)(unsafe.Pointer(&b.h.Cap))
 		// Create underlying byte array in the C memory, outside of GC's eyes.
 		C.cbb_init(ptrErr, ptrAddr, ptrCap)
 		if b.e != 0 {
 			return 0, errs[b.e]
 		}
 		// Manually create the byte slice.
-		b.b = *(*[]byte)(unsafe.Pointer(&b.sh))
+		b.b = *(*[]byte)(unsafe.Pointer(&b.h))
 	}
 
-	if b.sh.Len+b.l > b.sh.Cap {
+	if b.h.Len+b.l > b.h.Cap {
 		// Increase capacity of the byte array due to not enough space in it.
-		err := b.Grow((b.sh.Len + b.l) * 2)
+		err := b.Grow((b.h.Len + b.l) * 2)
 		if err != nil {
 			return 0, err
 		}
@@ -112,7 +112,7 @@ func (b *CByteBuf) Write(data []byte) (int, error) {
 	// Add data to the slice.
 	b.b = append(b.b, data...)
 	// Increase internal len for further grows.
-	b.sh.Len += b.l
+	b.h.Len += b.l
 
 	return b.l, errs[b.e]
 }
@@ -138,16 +138,16 @@ func (b *CByteBuf) Grow(cap int) error {
 		return errs[errNegativeCap]
 	}
 	// Save new capacity.
-	b.sh.Cap = cap
+	b.h.Cap = cap
 	ptrErr := (*C.uint)(unsafe.Pointer(&b.e))
-	ptrAddr := (*C.uintptr)(unsafe.Pointer(&b.sh.Data))
-	ptrCap := (*C.int)(unsafe.Pointer(&b.sh.Cap))
+	ptrAddr := (*C.uintptr)(unsafe.Pointer(&b.h.Data))
+	ptrCap := (*C.int)(unsafe.Pointer(&b.h.Cap))
 	// Reallocate underlying byte array in C memory.
 	// New array may overlap with the previous if it's possible to resize it (there is free space at the right side).
 	// All necessary copying/free will perform implicitly, don't worry about this.
 	C.cbb_grow(ptrErr, ptrAddr, ptrCap)
 	// Recreate the slice (old accumulated data keeps).
-	b.b = *(*[]byte)(unsafe.Pointer(&b.sh))
+	b.b = *(*[]byte)(unsafe.Pointer(&b.h))
 	return errs[b.e]
 }
 
@@ -155,7 +155,7 @@ func (b *CByteBuf) Grow(cap int) error {
 //
 // Delta may be negative, but if delta will less than -capacity, the error will be triggered.
 func (b *CByteBuf) GrowDelta(delta int) error {
-	return b.Grow(b.sh.Cap + delta)
+	return b.Grow(b.h.Cap + delta)
 }
 
 // Get the contents of the buffer.
@@ -171,7 +171,7 @@ func (b *CByteBuf) String() string {
 // Reset all data accumulated in buffer.
 func (b *CByteBuf) Reset() {
 	b.b = b.b[:0]
-	b.sh.Len = 0
+	b.h.Len = 0
 }
 
 // Manually release of the underlying byte array.
@@ -179,11 +179,11 @@ func (b *CByteBuf) Reset() {
 // Using the buffer data after call this func may crash your app.
 func (b *CByteBuf) Release() error {
 	ptrErr := (*C.uint)(unsafe.Pointer(&b.e))
-	ptrAddr := (*C.uintptr)(unsafe.Pointer(&b.sh.Data))
+	ptrAddr := (*C.uintptr)(unsafe.Pointer(&b.h.Data))
 	// Free memory.
 	C.cbb_release(ptrErr, ptrAddr)
 	// Truncate length and capacity.
-	b.sh.Len, b.sh.Cap = 0, 0
+	b.h.Len, b.h.Cap = 0, 0
 	// Slice is broken here, therefore kill it.
 	b.b = nil
 	return errs[b.e]
