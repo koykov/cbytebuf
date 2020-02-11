9a1574346031389019b93d8d6f383b6e84a03347
Konstantin Voykov <koykov0@gmail.com>
2020-02-11 23:10:49 +0200

    Temporarily use both pools for testing.

--- pool.go
+++ pool.go
@@ -1,20 +1,30 @@
 package cbytebuf
 
-import "github.com/koykov/lbpool"
+import (
+	"sync"
+
+	"github.com/koykov/lbpool"
+)
 
 // Simple byte buffer pool.
 type Pool struct {
+	p sync.Pool
+}
+
+type LBPool struct {
 	Size uint
 	p    lbpool.Pool
 }
 
 // Default instance of the pool for simple cases.
 // Just call cbytebuf.P.Get() and cbytebuf.P.Put().
-var P = Pool{Size: 1000}
+var (
+	P   Pool
+	LBP = LBPool{Size: 1000}
+)
 
 // Get old byte buffer from the pool or create a new byte buffer.
 func (p *Pool) Get() *CByteBuf {
-	p.p.Size = p.Size
 	v := p.p.Get()
 	if v != nil {
 		if b, ok := v.(*CByteBuf); ok {
@@ -28,6 +38,29 @@ func (p *Pool) Get() *CByteBuf {
 //
 // Using data returned from the buffer after putting is unsafe.
 func (p *Pool) Put(b *CByteBuf) {
+	if b.h.Data == 0 {
+		return
+	}
+	b.Reset()
+	p.p.Put(b)
+}
+
+// Get old byte buffer from the pool or create a new byte buffer.
+func (p *LBPool) Get() *CByteBuf {
+	p.p.Size = p.Size
+	v := p.p.Get()
+	if v != nil {
+		if b, ok := v.(*CByteBuf); ok {
+			return b
+		}
+	}
+	return &CByteBuf{}
+}
+
+// Put byte buffer back to the pool.
+//
+// Using data returned from the buffer after putting is unsafe.
+func (p *LBPool) Put(b *CByteBuf) {
 	if b.h.Data == 0 {
 		return
 	}
